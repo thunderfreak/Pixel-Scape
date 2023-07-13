1b0cce9a0d0b1100d0dee8e6e336a52e20a6c65d
thunderfreak <sohambhattacharjee0711@gmail.com>
2023-07-13 15:30:41 +0530

    initial comit

---
+++ src/components/Ground.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { usePlane } from "@react-three/cannon";
+import { grassTexture } from "../images/textures";
+import { useStore } from "../hooks/useStore";
+
+export const Ground = () => {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    position: [0, -0.5, 0],
+  }));
+  const [addCube] = useStore((state) => [state.addCube]);
+  grassTexture.repeat.set(100, 100);
+
+  return (
+    <mesh
+      onClick={(e) => {
+        e.stopPropagation();
+        const [x, y, z] = Object.values(e.point).map((val) => Math.ceil(val));
+        addCube(x, y, z);
+      }}
+      ref={ref}
+    >
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshStandardMaterial attach="material" map={grassTexture} />
+    </mesh>
+  );
+};
---
+++ src/images/textures.js
@@ -0,0 +1,35 @@
+import { NearestFilter, TextureLoader, RepeatWrapping } from 'three';
+
+import {
+    dirtImg,
+	grassImg,
+	glassImg,
+	woodImg,
+	logImg,
+} from './images';
+
+const dirtTexture = new TextureLoader().load(dirtImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const grassTexture = new TextureLoader().load(grassImg);
+const woodTexture = new TextureLoader().load(woodImg);
+const logTexture = new TextureLoader().load(logImg);
+const groundTexture = new TextureLoader().load(dirtImg);
+
+dirtTexture.magFilter = NearestFilter;
+logTexture.magFilter = NearestFilter;
+grassTexture.magFilter = NearestFilter;
+glassTexture.magFilter = NearestFilter;
+woodTexture.magFilter = NearestFilter;
+groundTexture.magFilter = NearestFilter;
+grassTexture.wrapS = RepeatWrapping
+grassTexture.wrapT = RepeatWrapping
+
+
+export {
+    dirtTexture,
+    glassTexture,
+    grassTexture,
+    woodTexture,
+    logTexture,
+    groundTexture
+};
